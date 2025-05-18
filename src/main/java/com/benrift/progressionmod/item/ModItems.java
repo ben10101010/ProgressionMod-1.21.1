b92d49e2d5936beae82246205541e43286553a4e
Zariftstaker <Theriftstaker@gmail.com>
2025-05-18 17:58:56 -0400

    Added Torture Device EOP Working on creative tab

--- src/main/java/com/benrift/progressionmod/item/ModItems.java
+++ src/main/java/com/benrift/progressionmod/item/ModItems.java
@@ -3,6 +3,7 @@ package com.benrift.progressionmod.item;
 import com.benrift.progressionmod.ProgressionMod;
 import com.benrift.progressionmod.block.ModBlocks;
 import com.benrift.progressionmod.item.CustomItems.FruitOfConsequences;
+import com.benrift.progressionmod.item.CustomItems.TortureDevice;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
@@ -15,10 +16,19 @@ import java.util.function.Supplier;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ProgressionMod.MOD_ID);
 
-    public static final Supplier<Item> FRUIT_OF_CONSEQUENCE = ITEMS.register("fruit_of_consequence", () -> new FruitOfConsequences(new Item.Properties().stacksTo(4).
-            rarity(Rarity.EPIC).food((new FoodProperties.Builder()).nutrition(40).saturationModifier(0.3f).alwaysEdible().build())));
+    public static final Supplier<Item> FRUIT_OF_CONSEQUENCE = ITEMS.register("fruit_of_consequence",
+            () -> new FruitOfConsequences(new Item.Properties()
+                    .stacksTo(4)
+                    .rarity(Rarity.EPIC)
+                    .food(new FoodProperties.Builder().nutrition(40).saturationModifier(0.3f).alwaysEdible().build())));
 
-    public static void register(IEventBus eventBus){
+    public static final Supplier<Item> TORTURE_DEVICE = ITEMS.register("torture_device",
+            () -> new TortureDevice(new Item.Properties()
+                    .stacksTo(1)
+                    .rarity(Rarity.EPIC)
+                    .food(new FoodProperties.Builder().nutrition(40).saturationModifier(0.3f).alwaysEdible().build())));
+
+    public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
 }
